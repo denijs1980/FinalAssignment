23b56f1ddabd261f9715a12391d29b3df188d1b2
denijs1980 <107810072+denijs1980@users.noreply.github.com>
2022-08-09 15:28:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+
+string [] startingArray = new string[] {"piece", "chunk", "8", "***"};
+string[] resultArray = new string [0];
+
+foreach (var item in startingArray)
+{
+    if (item.Length <= 3)
+    {
+    Array.Resize(ref resultArray, resultArray.Length + 1);
+    resultArray [resultArray.Length-1] = item;
+    }
+}
+
+Console.WriteLine($"[{String.Join("; ", resultArray)}]");
